dd733ae0cfcb4b5f6c02851293c7faae82333d52
drei109 <dreirar@gmail.com>
2017-05-08 17:07:25 -0500

    Agregar archivos de proyecto.

---
+++ Gimnasio.Dominio/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gimnasio.Dominio
+{
+    public class Rutina
+    {
+        public int id_rutina { get; set; }
+        public int nro_repeticiones { get; set; }
+        public string descripcion_rutina { get; set; }
+
+        public static Rutina crear(string descripcion)
+        {
+            return new Rutina()
+            {
+
+                descripcion_rutina = descripcion
+            };
+        }
+    }
+}
