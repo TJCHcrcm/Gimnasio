7be2af85d42930b6c0816cc2de400872d25d60c0
Usuario <Usuario@BEPIS06>
2017-05-16 17:51:00 -0500

    Tabla tipo ejercicio

---
+++ Gimnasio.Dominio/Entidades/Tipo_Ejercicio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gimnasio.Dominio.Entidades
+{
+    class Tipo_Ejercicio
+    {
+        #region Propiedades
+        public int id_ejercicio { get; set; }
+        public string nombre_ejercicio { get; set; }
+        public string musculo_a_trabajar { get; set; }
+        public string tipo_ejercicio { get; set; }
+        public string num_repeticiones { get; set; }
+
+        #endregion
+        #region Constructor
+        public Tipo_Ejercicio() {
+        }
+        #endregion
+        #region Metodos
+        
+        #endregion
+    }
+}
