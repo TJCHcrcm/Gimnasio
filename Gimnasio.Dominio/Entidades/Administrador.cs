560de3d0556af391fc021ee327752ac5dcb7f863
BEPIS06 <BEPIS06>
2017-05-17 16:22:29 -0500

    agregado clase Administrador

---
+++ Gimnasio.Dominio/Entidades/Administrador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gimnasio.Dominio.Entidades
+{
+    class Administrador
+    {
+        #region Propiedades
+
+        public int id_tipo_usuario { get; set; }
+        public string horario { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        private Administrador()
+        {
+        }
+
+        #endregion
+
+        #region Metodos
+
+        public static Administrador Crear(int id_tipo_usuario_mod, string horario_mod)
+        {
+            return new Administrador()
+            {
+                id_tipo_usuario = id_tipo_usuario_mod,
+                horario = horario_mod
+            };
+        }
+        public void Modificaradministrador(int id_tipo_usuario_mod, string horario_mod)
+        {
+            id_tipo_usuario = id_tipo_usuario_mod;
+            horario = horario_mod;
+        }
+        #endregion
+    }
+}
